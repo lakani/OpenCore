38d660c324795828f335d7400204a206d6d538d5
Ahmed al Lakani <ahmed.al.lakani@gmail.com>
2019-12-17 14:34:42 +0200

    Testing

--- SQL/Test/Testing_fn_GetGLInfo.sql
+++ SQL/Test/Testing_fn_GetGLInfo.sql
@@ -1,10 +1,18 @@
 
-USE [OpenCore]
-GO
+
 
 select * from VW_DEF_GL
 
-select * from [fn_GetGLInfo]
+
+USE [OpenCore]
+GO
+select GLS.GL , GLIn. from VW_DEF_GL GLS CROSS APPLY [dbo].[fn_GetGLInfo] ( GLS.GL, GLS.CURR) GLIn
+
+IF (  (	select COUNT(GLIn.CompanyNo) 
+			from @Legs ls CROSS APPLY [dbo].[fn_GetGLInfo] ( ls.Acct_No, @BaseCurr) GLIn
+			where ls.GL = 1 and GLIn.CompanyNo != @nCompany) <> 0)
+		RETURN (6)
+
 
 SELECT * FROM [dbo].[fn_GetGLInfo] ('01-01-01-01-01-01-00001' , 'EGP')
 GO
