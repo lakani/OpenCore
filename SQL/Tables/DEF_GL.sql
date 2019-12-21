69eea45957096c7b0af619405b137cf90e9f838a
Lakani <ahmed.al.lakani@gmail.com>
2019-12-21 14:05:48 +0200

    new Changes in Table [dbo].[GL_DEF]

--- SQL/Tables/DEF_GL.sql
+++ SQL/Tables/DEF_GL.sql
@@ -1,29 +1,37 @@
 USE [OpenCore]
 GO
 
-/****** Object:  Table [dbo].[DEF_GL]    Script Date: 17/12/2019 13:20:51 ******/
+/****** Object:  Table [dbo].[GL_DEF]    Script Date: 21/12/2019 2:04:16 pm ******/
+DROP TABLE [dbo].[GL_DEF]
+GO
+
+/****** Object:  Table [dbo].[GL_DEF]    Script Date: 21/12/2019 2:04:16 pm ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[DEF_GL](
-	[Zone] [tinyint] NOT NULL,
-	[CompanyNo] [smallint] NOT NULL,
-	[BranchNo] [smallint] NOT NULL,
-	[SectorNo] [tinyint] NOT NULL,
-	[DepNo] [tinyint] NOT NULL,
-	[UnitNO] [tinyint] NOT NULL,
-	[Nature] [tinyint] NOT NULL,
-	[PostingLevel] [tinyint] NOT NULL,
+CREATE TABLE [dbo].[GL_DEF](
+	[GL_DEFID] [int] NOT NULL,
+	[Zone] [int] NOT NULL,
+	[CompanyNo] [int] NOT NULL,
+	[BranchNo] [int] NOT NULL,
+	[SectorNo] [int] NOT NULL,
+	[DepNo] [int] NOT NULL,
+	[UnitNO] [int] NOT NULL,
+	[Type] [int] NOT NULL,
+	[PostingLevel] [int] NOT NULL,
 	[LedgerNO] [nvarchar](15) NOT NULL,
 	[CURR] [nvarchar](4) NOT NULL,
-	[TotallingGL] [nvarchar](35) NULL,
 	[COMMENTS] [nvarchar](200) NULL,
 	[EFFECTIVE_DT] [datetime] NOT NULL,
-	[STATUS] [tinyint] NOT NULL,
-	[REFERENCE] [nvarchar](max) NULL
+	[STATUS] [smallint] NOT NULL,
+	[REFERENCE] [nvarchar](max) NULL,
+ CONSTRAINT [PK_GL_DEF] PRIMARY KEY CLUSTERED 
+(
+	[GL_DEFID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
