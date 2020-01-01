d87a3762d7da9ea22b3718484836bf502613b8d8
Lakani <ahmed.al.lakani@gmail.com>
2020-01-01 20:06:40 +0200

    changes in the DB

--- SQL/Tables/DEF_GL.sql
+++ SQL/Tables/DEF_GL.sql
@@ -35,3 +35,52 @@ CREATE TABLE [dbo].[GL_DEF](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
+
+
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.Tmp_DEF_GL
+	(
+	GL_DEFID int NOT NULL IDENTITY (1, 1),
+	Zone tinyint NOT NULL,
+	CompanyNo smallint NOT NULL,
+	BranchNo smallint NOT NULL,
+	SectorNo tinyint NOT NULL,
+	DepNo tinyint NOT NULL,
+	UnitNO tinyint NOT NULL,
+	Nature tinyint NOT NULL,
+	PostingLevel tinyint NOT NULL,
+	LedgerNO nvarchar(15) NOT NULL,
+	CURR nvarchar(4) NOT NULL,
+	TotallingGL nvarchar(35) NULL,
+	COMMENTS nvarchar(200) NULL,
+	EFFECTIVE_DT datetime NOT NULL,
+	STATUS tinyint NOT NULL,
+	REFERENCE nvarchar(MAX) NULL
+	)  ON [PRIMARY]
+	 TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE dbo.Tmp_DEF_GL SET (LOCK_ESCALATION = TABLE)
+GO
+SET IDENTITY_INSERT dbo.Tmp_DEF_GL OFF
+GO
+IF EXISTS(SELECT * FROM dbo.DEF_GL)
+	 EXEC('INSERT INTO dbo.Tmp_DEF_GL (Zone, CompanyNo, BranchNo, SectorNo, DepNo, UnitNO, Nature, PostingLevel, LedgerNO, CURR, TotallingGL, COMMENTS, EFFECTIVE_DT, STATUS, REFERENCE)
+		SELECT Zone, CompanyNo, BranchNo, SectorNo, DepNo, UnitNO, Nature, PostingLevel, LedgerNO, CURR, TotallingGL, COMMENTS, EFFECTIVE_DT, STATUS, REFERENCE FROM dbo.DEF_GL WITH (HOLDLOCK TABLOCKX)')
+GO
+DROP TABLE dbo.DEF_GL
+GO
+EXECUTE sp_rename N'dbo.Tmp_DEF_GL', N'DEF_GL', 'OBJECT' 
+GO
+COMMIT
