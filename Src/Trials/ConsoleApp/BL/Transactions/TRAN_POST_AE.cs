929c9ae8cb2a94a0295d50580c75f6b4079473d7
Ahmed al Lakani <ahmed.al.lakani@gmail.com>
2020-01-06 16:40:30 +0200

    Daily Checkin

--- Src/Trials/ConsoleApp/BL/Transactions/TRAN_POST_AE.cs
+++ Src/Trials/ConsoleApp/BL/Transactions/TRAN_POST_AE.cs
@@ -14,13 +14,41 @@ namespace SIS.OpenCore.BL.Transactions
         static public Guid Post(TRAN_POST_AE_TYPE_PARAM[] ae_Param )        
         {
             string stBaseCurrency;
+            DateTime MaxEffDt = DateTime.MinValue;
+	        DateTime CRT_DT = DateTime.MinValue;
 
+            // Get Base Currency
             stBaseCurrency = Settings.fn_OPT_GetBaseCurrency();
+            if (string.IsNullOrEmpty(stBaseCurrency))
+                return Guid.Empty;
             
             if(Retrive_GL_Info(ae_Param,stBaseCurrency) == false)
                 return Guid.Empty;
 
             return new Guid();
+        }
+
+        /*
+                Ensure that sum of CR = SUM of DR
+	            select @SumOfCR = SUM(l.Acct_Amt) from @Legs l where UPPER (l.Acct_CR_DR) = 'CR'
+	            select @SumOfDR = SUM(l.Acct_Amt) from @Legs l where UPPER (l.Acct_CR_DR) = 'DR'
+        */
+        static public bool sum_of_CR_Equal_SUM_of_DR(TRAN_POST_AE_TYPE_PARAM[] Legs, string stBaseCurr)
+        {
+            decimal  SumOfCR = 0,  @SumOfDR = 0;
+
+            SumOfCR =   (from l in Legs
+                        where l.Acct_CR_DR == "CR" && l.Acct_Curr == stBaseCurr
+                        select l.Acct_Amt).Sum();
+
+            SumOfDR =   (from l in Legs
+                        where l.Acct_CR_DR == "DR" && l.Acct_Curr == stBaseCurr
+                        select l.Acct_Amt).Sum();
+
+            if (SumOfDR != SumOfCR)
+                return false;
+            else
+                return true;
         }
 
         // Ensure that all legs are within the same company
@@ -30,7 +58,7 @@ namespace SIS.OpenCore.BL.Transactions
             {
                 if (Leg.GL == true)
                     Leg.GL_Info = GL.fn_GetGLInfo(Leg.Acct_No, stBaseCurr);
-                if (Leg.GL_Info == null)
+                if (Leg.GL == true && Leg.GL_Info == null)
                     return false;
             }
 
