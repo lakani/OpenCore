e1ae446bfe3a82b67e5a232532af45baaafe5321
Ahmed al Lakani <ahmed.al.lakani@gmail.com>
2020-03-10 13:33:14 +0200

    Daily Checkin

--- SQL/Test/Testing_fn_GetGLInfo.sql
+++ SQL/Test/Testing_fn_GetGLInfo.sql
@@ -18,3 +18,15 @@ SELECT * FROM [dbo].[fn_GetGLInfo] ('01-01-01-01-01-01-00001' , 'EGP')
 GO
 
 
+
+select * from OpenCore..LUT_GL_ACCT_CATEGORY
+
+select top 1 GLNum from OpenCore..DEF_CK_ACCT_ACCT_STRUCT order by AccountStructID desc
+
+select top 1 l.Balance_After, l.*
+from OpenCore..TRN_LEGS l
+where   l.GL = 1 and l.STATUS_ID = 1  
+--l.Acct_No == stACCT_NO and l.Acct_Curr == Acct_Curr 
+order by l.EffDt desc , l.CREATE_DT desc, 
+l.Sequence desc, l.TRN_LEGS_ID desc
+                            
