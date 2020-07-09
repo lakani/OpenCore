35a50b6058460fda7c362d1146e6528682d1c8e4
ahmed.al.lakani@gmail.com <ahmed.al.lakani@gmail.com>
2020-07-09 15:39:47 +0200

    swagger modifications

--- Src/Trials/ConsoleApp/webapi/Startup.cs
+++ Src/Trials/ConsoleApp/webapi/Startup.cs
@@ -51,21 +51,10 @@ namespace SIS.OpenCore.webapi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                //c.RoutePrefix = string.Empty;
             });
 
             app.UseHttpsRedirection();
-
-             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
-            });
 
             app.UseRouting();
 
