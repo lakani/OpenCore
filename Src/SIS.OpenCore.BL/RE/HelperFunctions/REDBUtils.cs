644189c2a433beac44eac78e0e8a3822aa7e6831
ahmed.al.lakani@gmail.com <ahmed.al.lakani@gmail.com>
2021-02-10 15:43:46 +0200

    from Office, Initially working, but still working on the rest of validations

---
+++ Src/SIS.OpenCore.BL/RE/HelperFunctions/REDBUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIS.OpenCore.DAL.Context;
+
+namespace SIS.OpenCore.BL.RE.Helper
+{
+    public static class REDBUtils
+    {
+        public static bool CheckContains(string check, string valList)
+        {
+            if (String.IsNullOrEmpty(check) || String.IsNullOrEmpty(valList))
+                return false;
+
+            var list = valList.Split(',').ToList();
+            return list.Contains(check);
+        }
+
+        public static bool Exists(string table, string ID, string value)
+        {
+            string stSQL;
+            var context = new OpenCoreContext();
+
+            //select ID from DEF_CIF WHERE ID = 1203
+            stSQL = "select " + ID + " from " + table + " WHERE " + ID + " = " + value;
+            //context.Database.ExecuteSqlInterpolatedAsync()
+            return true;
+        }
+    }
+}
---
+++ Src/SIS.OpenCore.BL/RE/SmartRulesEngine.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using RulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using static RulesEngine.Extensions.ListofRuleResultTreeExtension;
+
+namespace SIS.OpenCore.BL.RE
+{
+    class SmartRulesEngine
+    {
+        public static void Run(object inputOne)
+        {
+            #region load Rule file and load it
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory() + "", "CIFRules.json", SearchOption.AllDirectories);
+            if (files == null || files.Length == 0)
+                throw new Exception("Rules not found.");
+            var fileData = File.ReadAllText(files[0]);
+            var workflowRules = JsonConvert.DeserializeObject<List<WorkflowRules>>(fileData);
+            var bre = new RulesEngine.RulesEngine(workflowRules.ToArray(), null);
+            #endregion
+
+            #region load paramters and run engine
+            var inputs = new RuleParameter[1];
+            // this is the mapping between the name in expression and the object
+            inputs[0] = new RuleParameter("CIF", inputOne);
+            
+            List<RuleResultTree> resultList = bre.ExecuteAllRulesAsync("CIFPOST", inputs).Result;
+            #endregion
+
+            #region check the return of rules
+            foreach(var ruleRet in resultList)
+            {
+                if(ruleRet.IsSuccess == false)
+                {
+                    throw new Exception(ruleRet.ExceptionMessage);
+                }
+            }
+
+            #endregion
+        }
+    }
+}
