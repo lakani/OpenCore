0feb25e93fb4bb482a1e0b38c5e3e25c971aa633
ahmed.al.lakani@gmail.com <ahmed.al.lakani@gmail.com>
2021-02-25 16:46:54 +0200

    restructure

---
+++ Src/SIS.OpenCore.Client.Http/Adapters/Objects/CIF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SIS.OpenCore.Model;
+using SIS.OpenCore.Shared.Objects;
+
+namespace SIS.OpenCore.Client.Http.Adapters.Objects
+{
+    public partial  class CIF
+    {
+        static public string Add_CIF(DateTime dtEFFECTIVE_DT,
+            short nCompanyNo, DEF_CIF _CIF)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DEF_CIF[] List(DEF_CIF_PARAM param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
